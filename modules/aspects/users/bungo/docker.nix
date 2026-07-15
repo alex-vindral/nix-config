ad4c88492271936cb33c0e2947e2b84d08d88989
Alex Bergdahl <alex.bergdahl@vindral.com>
2026-07-15 13:12:41 +0200

    Add wsl docker support

--- modules/aspects/users/bungo/docker.nix
+++ modules/aspects/users/bungo/docker.nix
@@ -1,33 +1,38 @@
-{bungo, ...}: {
-  bungo.aspects.docker = {host, ...}: {
+{bungo, lib, ...}: {
+  bungo.aspects.docker = {host, ...}: let
+    isWsl = (host.wsl or {}).enable or false;
+  in {
     includes = [
+      bungo.aspects.sops
       (
-        if (host.wsl or {}).enable or false
+        if isWsl
         then bungo.aspects.docker-desktop
         else bungo.aspects.docker-native
       )
     ];
 
-    # TODO: needs bungo.aspects.sops; re-enable once WSL can decrypt secrets.
-    # homeManager = {
-    #   config,
-    #   pkgs,
-    #   ...
-    # }: {
-    #   sops.secrets."docker/ghcr_token" = {};
-    #
-    #   systemd.user.services.ghcr-login = {
-    #     Unit = {
-    #       Description = "Log Docker in to ghcr.io using the ghcr token";
-    #       After = ["sops-nix.service"];
-    #       Requires = ["sops-nix.service"];
-    #     };
-    #     Service = {
-    #       Type = "oneshot";
-    #       ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/cat ${config.sops.secrets."docker/ghcr_token".path} | ${pkgs.docker}/bin/docker login ghcr.io -u x-access-token --password-stdin'";
-    #     };
-    #     Install.WantedBy = ["default.target"];
-    #   };
-    # };
+    homeManager = {
+      config,
+      pkgs,
+      ...
+    }: {
+      sops.secrets."docker/ghcr_token" = {};
+
+      systemd.user.services.ghcr-login = {
+        Unit = {
+          Description = "Log Docker in to ghcr.io using the ghcr token";
+          After = ["sops-nix.service"];
+          Requires = ["sops-nix.service"];
+        };
+        Service = {
+          Type = "oneshot";
+          # Docker Desktop's credsStore shells out to a Windows helper on the
+          # interop PATH, which the service doesn't otherwise inherit.
+          Environment = lib.optional isWsl ''"PATH=/mnt/c/Program Files/Docker/Docker/resources/bin"'';
+          ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/cat ${config.sops.secrets."docker/ghcr_token".path} | ${pkgs.docker}/bin/docker login ghcr.io -u x-access-token --password-stdin'";
+        };
+        Install.WantedBy = ["default.target"];
+      };
+    };
   };
 }
